84907e85822af404f492f835b33ff5f332c3d8f7
Francesco Nisi <francesconisi95@gmail.com>
2021-06-08 16:52:17 +0200

    data not found

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -1,1 +1,40 @@
 @extends('layouts.app')
+
+@section('main_placeholder')
+
+    <div class="main-container">
+
+        <div class="wrapper">
+
+            {{-- Current Series --}}
+            <div class="current-series">
+
+                <span>CURRENT SERIES</span>
+
+            </div>
+            {{-- End Current Series --}}
+
+            {{--Comics--}}
+            <div class="comics-container">
+
+                @foreach ($data as $item)
+
+                    <div class="comic-card">
+
+                        <img src="{{$item['thumb']}}" alt="{{$item['title']}}">
+
+                        <span>{{$item['title']}}</span>
+
+                    </div>
+
+                @endforeach
+
+            </div>
+            {{--Comics--}}
+
+        </div>
+
+    </div>
+
+@endsection
+
--- routes/web.php
+++ routes/web.php
@@ -15,9 +15,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
 
-    $data = config('comics');
+    $comics = config('comics');
+
+    $data = $comics;
 
     //dd($data);
 
     return view('homepage', $data);
-});
+
+})->name('homepage');
