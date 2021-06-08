b79d390bce37b4f6d28a5ee1c8ebb9faea919c48
Francesco Nisi <francesconisi95@gmail.com>
2021-06-08 18:09:02 +0200

    inizio footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,5 +1,23 @@
 <footer>
 
-    
+    <div class="footer-continer">
+
+        <div class="wrapper">
+
+            {{-- Footer Links --}}
+            <div class="footer-links-container">
+                <img src="{{asset('img/dc-logo-bg.png')}}" alt="dc-logo-cover">
+            </div>
+            {{-- Footer Links --}}
+
+            {{-- Background cover logo --}}
+            <div class="background-cover-logo-container">
+                <img src="{{asset('img/dc-logo-bg.png')}}" alt="dc-logo-cover">
+            </div>
+            {{-- Background cover logo --}}
+
+        </div>
+
+    </div>
 
 </footer>
