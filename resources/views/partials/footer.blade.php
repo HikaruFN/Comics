7128691ef8e1bfdf6d8eaef6e6465b722909f95f
Francesco Nisi <francesconisi95@gmail.com>
2021-06-08 20:15:59 +0200

    End Footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -4,20 +4,128 @@
 
         <div class="wrapper">
 
-            {{-- Footer Links --}}
-            <div class="footer-links-container">
-                <img src="{{asset('img/dc-logo-bg.png')}}" alt="dc-logo-cover">
-            </div>
-            {{-- Footer Links --}}
+            <div class="footer-content-container">
 
-            {{-- Background cover logo --}}
-            <div class="background-cover-logo-container">
-                <img src="{{asset('img/dc-logo-bg.png')}}" alt="dc-logo-cover">
+                {{-- Footer Links --}}
+                <div class="footer-links-container">
+
+                    <div class="double-link-container">
+
+                        <div class="link-container">
+
+                            <h3>DC COMICS</h3>
+
+                            <ul>
+                                <li>Characters</li>
+                                <li>Comics</li>
+                                <li>Movies</li>
+                                <li>TV</li>
+                                <li>Games</li>
+                                <li>Videos</li>
+                                <li>News</li>
+                            </ul>
+                            
+                        </div>
+
+                        <div class="link-container">
+
+                            <h3>SHOP</h3>
+
+                            <ul>
+                                <li>Shop DC</li>
+                                <li>Shop DC Collectables</li>
+                            </ul>
+                        
+                        </div>
+
+                    </div>
+
+                    <div class="link-container">
+
+                        <h3>DC</h3>
+
+                        <ul>
+                            <li>Terms Of Use</li>
+                            <li>Provacy policu(New)</li>
+                            <li>Ad Choices</li>
+                            <li>Advertising</li>
+                            <li>Jobs</li>
+                            <li>Subscriptions</li>
+                            <li>Talent Workshops</li>
+                            <li>CPSC Certificates</li>
+                            <li>Ratings</li>
+                            <li>Shop Help</li>
+                            <li>Contact Us</li>
+                        </ul>
+                        
+                    </div>
+
+                    <div class="link-container">
+                        <h3>SITES</h3>
+
+                        <ul>
+                            <li>DC</li>
+                            <li>MAD Magazine</li>
+                            <li>DC Kids</li>
+                            <li>DC Universe</li>
+                            <li>DC Power Visa</li>
+                        </ul>
+                    </div>
+
+                </div>
+                {{-- End Footer Links --}}
+
+                {{-- Cover logo --}}
+                <div class="cover-logo-container">
+
+                    <img src="{{ asset('img/dc-logo-bg.png') }}" alt="">
+                    
+                </div>
             </div>
-            {{-- Background cover logo --}}
+
+            {{-- Reserved Rights --}}
+
+            <div class="reserved-rights-container">
+                <p>All Site Content TM and 2020 DC Entertainment, unless otherwise <a href="#">noted here</a>.All Rights reserved.<br><a href="#">Cookies Settings</a></p>
+            </div>
+
+            {{-- Reserved Rights --}}
 
         </div>
 
     </div>
+
+    {{-- Bottom Footer --}}
+
+    <div class="bottom-footer-container">
+
+        <div class="wrapper">
+
+            <div class="bottom-footer-content-container">
+
+                {{-- Singn up  --}}
+                <div class="sign-up-container">
+                    <a href="#">SING-UP NOW!</a>
+                </div>
+                {{-- End Singn up  --}}
+
+                {{-- Socials --}}
+                <div class="socials-container">
+                    <span>FOLLOW US</span>
+                    <img src="{{asset('img/footer-facebook.png')}}" alt="facebook">
+                    <img src="{{asset('img/footer-twitter.png')}}" alt="twitter">
+                    <img src="{{asset('img/footer-youtube.png')}}" alt="youtube">
+                    <img src="{{asset('img/footer-pinterest.png')}}" alt="pinterest">
+                    <img src="{{asset('img/footer-periscope.png')}}" alt="periscope">
+                </div>
+                {{-- End Socials --}}
+
+            </div>
+
+        </div>
+
+    </div>
+
+    {{-- Bottom Footer --}}
 
 </footer>
